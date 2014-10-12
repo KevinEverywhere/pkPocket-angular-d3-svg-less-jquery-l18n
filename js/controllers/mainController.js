6172714898f37390ef6c37624fde794586b69f4c
Kevin Ready <kevin.ready@gmail.com>
2014-10-11 23:05:21 -0700

    Cleaning up css

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -33,32 +33,3 @@ masterMapApp.controller('MainCtrl',
 	}
 ]);
 
-/*
-
-masterMapApp.controller('MainCtrl', 
-	["$scope", "$rootScope", "$http", "$stateParams", "$q", "MapService", "D3Service",
-	function MainCtrl($scope, $rootScope, $http, $stateParams, $q, MapService, D3Service) {
-		var countries=$rootScope.countries;
- 		$scope.setCountry=function (cID){
-			$scope.countryName="";
-			console.log('MapService=' + MapService)
-			MapService.getCountryData(cID);
-			$scope.$broadcast("countrySelected");
-			$rootScope.$broadcast("countrySelected",{Country:$rootScope.countries[cID]});
-		}
- 		$scope.$on('countrySelected',function(){
-			$scope.countryName="";
- 		});
-		if($rootScope.countries.length==0){
-			MapService.init($scope);
-		//	D3Service.init($scope);
-		}else{
-			if($stateParams.CountryID){
-				console.log('MainCtrl.MapService.setCountry=' +  $stateParams.CountryID)
-				MapService.getCountryData($stateParams.CountryID);
-				$rootScope.$broadcast("countrySelected",{Country:$rootScope.countries[$stateParams.CountryID]});
-			}
-		}
-	}
-]);
-*/
